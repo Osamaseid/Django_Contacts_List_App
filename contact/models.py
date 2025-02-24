82ef25a19ccc59740fd56d2ca626cd4552bf9ef1
osama seid <osaseid1@gmail.com>
2025-02-24 15:45:51 +0300

    contact list app deliverd

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Contact(models.Model):
+    full_name = models.CharField(max_length=200)
+    relationship = models.CharField(max_length=50)
+    email = models.EmailField(max_length=100)
+    phone_number = models.CharField(max_length=20)
+    address = models.TextField(max_length = 200)
+
+    def __str__(self):
+        return self.full_name
+# Compare this snippet from contactlist/urls.py:
